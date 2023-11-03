c7dc887a1bcd46773d69c73b50e8c7e11fdbd260
sawara711 <uekibati.su@gmail.com>
2023-11-03 23:56:54 +0900

    patternsappコントローラー/ビュー

---
+++ app/controllers/patterns_controller.rb
@@ -0,0 +1,14 @@
+class PatternsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    #@patterns = Pattern.all
+  end
+
+  def new
+    #@pattern = Pattern.new
+  end
+
+  private
+
+end
